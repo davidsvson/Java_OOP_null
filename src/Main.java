a9e621f737682dc689c25cd437b7f621f43b7c75
David Svensson <davidsvson@gmail.com>
2022-09-16 10:19:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import org.w3c.dom.css.Rect;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rec = new Rectangle(3.0, 4.0);
+        Rectangle rec2 = new Rectangle(3.0, 4.0);
+        Rectangle rec3 = new Rectangle(3.0, 4.0);
+        Rectangle rec4 = new Rectangle(3.0, 4.0);
+
+        rec3 = null;
+        ArrayList<Rectangle> recList = new ArrayList<>();
+
+        recList.add(rec);
+        recList.add(rec2);
+        recList.add(rec3);
+        recList.add(rec4);
+
+
+
+        for(Rectangle rectangle : recList ) {
+            if (rectangle != null) {
+                System.out.println(rectangle.getHeight());
+            }
+        }
+
+
+    }
+}
